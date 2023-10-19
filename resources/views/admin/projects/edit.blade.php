5013369b0593e39f4b11f9846a7c295b0c5e5938
AndreaLinza <andrealinza@gmail.com>
2023-10-19 17:33:13 +0200

    fix

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -7,23 +7,27 @@
 
     <div class="container">
         <h1>Edit della Repo</h1>
+        <div class="d-flex">
+            <div>
+                {{-- Utilizziamo l'include del file blade upsert per poter utilizzare un form in più pagine --}}
+                @include('admin.projects.forms.upsert', [
+                    // assegniamo all'action la rotta da seguire al form
+                    'action' => route('admin.projects.update', $project->slug),
+                    //assegniamo il method per utilizzarlo nel form
+                    'method' => 'PUT',
+                    'project' => $project,
+                ])
+            </div>
+            <div>
+                {{-- Utilizziamo questo form per eliminare la repo(in quetso caso) --}}
+                <form action="{{ route('admin.projects.destroy', $project->slug) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
 
-        {{-- Utilizziamo l'include del file blade upsert per poter utilizzare un form in più pagine --}}
-        @include('admin.projects.forms.upsert', [
-            // assegniamo all'action la rotta da seguire al form
-            'action' => route('admin.projects.update', $project->slug),
-            //assegniamo il method per utilizzarlo nel form
-            'method' => 'PUT',
-            'project' => $project,
-        ])
-
-        {{-- Utilizziamo questo form per eliminare la repo(in quetso caso) --}}
-        <form action="{{ route('admin.projects.destroy', $project->slug) }}" method="POST">
-            @csrf
-            @method('DELETE')
-
-            <button class="my-small-button"><span>Elimina</span></button>
-        </form>
+                    <button class="my-small-button"><span>Elimina</span></button>
+                </form>
+            </div>
+        </div>
     </div>
 
 @endsection
